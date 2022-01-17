c232ccc65ea32ba61b07de24ec8894e59198316b
Yusuf Luai <yusuf.luai01@gmail.com>
2022-01-18 02:38:36 +0700

    Commit staging branch

--- terraform/jenkins/jenkins-instance/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "instance_public_ip"{
-    value = aws_instance.jenkins_instance.public_ip
-}
--- terraform/sg/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "main_security_group_id" {
-  value = module.main_sg.security_group_id
-}
-
-output "db_security_group_id" {
-  value = module.db_sg.security_group_id
-}
